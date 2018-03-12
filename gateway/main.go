d270fec1adc0753ec90f2ac75512a79bf27e5d80
Benjamin Misell <benjamin@misell.cymru>
2018-03-12 13:27:31 +0000

    Extra fields to hotel query

--- gateway/main.go
+++ gateway/main.go
@@ -203,6 +203,23 @@ var hotelType = graphql.NewObject(graphql.ObjectConfig{
 				return nil, nil
 			},
 		},
+		"checkIn": &graphql.Field{
+			Type: graphql.DateTime,
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				hotel, isOk := params.Source.(map[string]interface{})
+				if isOk {
+					checkIn, isOk := hotel["checkIn"].(string)
+					if isOk {
+						time, err := time.Parse(time.RFC3339, checkIn)
+						if err != nil {
+							return nil, err
+						}
+						return time, nil
+					}
+				}
+				return nil, nil
+			},
+		},
 		"name": &graphql.Field{
 			Type: graphql.String,
 			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
@@ -211,6 +228,19 @@ var hotelType = graphql.NewObject(graphql.ObjectConfig{
 					name, isOk := hotel["name"].(string)
 					if isOk {
 						return name, nil
+					}
+				}
+				return nil, nil
+			},
+		},
+		"address": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				hotel, isOk := params.Source.(map[string]interface{})
+				if isOk {
+					address, isOk := hotel["address"].(string)
+					if isOk {
+						return address, nil
 					}
 				}
 				return nil, nil
