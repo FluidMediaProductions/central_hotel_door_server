bb9b278cb7357afbf51e957bce2a09dc1731d8d3
Benjamin Misell <benjamin@misell.cymru>
2018-03-22 13:00:09 +0000

    Send room open actions to hotel

--- hotel_gateway/main.go
+++ hotel_gateway/main.go
@@ -54,6 +54,10 @@ var protoHandlers = []ProtoHandler{
 		msgType: hotel_comms.MsgType_HOTEL_PING,
 		handler: hotelPing,
 	},
+	{
+		msgType: hotel_comms.MsgType_GET_ACTIONS,
+		handler: getAction,
+	},
 }
 
 func protoServ(w http.ResponseWriter, r *http.Request) {
@@ -202,6 +206,34 @@ func getRoomsByHotel(hotel uint) ([]interface{}, error) {
 	return nil, nil
 }
 
+func getActions(hotelId uint) ([]*hotel_comms.Action, error) {
+	actions := make([]*hotel_comms.Action, 0)
+	rooms, err := getRoomsByHotel(hotelId)
+	if err != nil {
+		return nil, err
+	}
+	for _, room := range rooms {
+		room, isOk := room.(map[string]interface{})
+		if isOk {
+			shouldOpen, isOk := room["shouldOpen"].(bool)
+			if isOk {
+				if shouldOpen {
+					id, isOk := room["ID"].(float64)
+					if isOk {
+						actionType := hotel_comms.ActionType_ROOM_UNLOCK
+						action := &hotel_comms.Action{
+							Type: &actionType,
+							Id:   proto.Int64(int64(id)),
+						}
+						actions = append(actions, action)
+					}
+				}
+			}
+		}
+	}
+	return actions, nil
+}
+
 func hotelPing(hotel *HotelServer, msg []byte, sig []byte, w http.ResponseWriter) error {
 	newMsg := &hotel_comms.HotelPing{}
 	err := proto.Unmarshal(msg, newMsg)
@@ -225,24 +257,11 @@ func hotelPing(hotel *HotelServer, msg []byte, sig []byte, w http.ResponseWriter
 	hotel.Online = true
 	db.Save(hotel)
 
-	actionRequired := false
-
-	rooms, err := getRoomsByHotel(hotel.HotelId)
+	actions, err := getActions(hotel.HotelId)
 	if err != nil {
 		return err
 	}
-	for _, room := range rooms {
-		room, isOk := room.(map[string]interface{})
-		if isOk {
-			shouldOpen, isOk := room["shouldOpen"].(bool)
-			if isOk {
-				if shouldOpen {
-					actionRequired = true
-					break
-				}
-			}
-		}
-	}
+	actionRequired := len(actions) != 0
 
 	resp := &hotel_comms.HotelPingResp{
 		Success:        proto.Bool(true),
@@ -251,6 +270,27 @@ func hotelPing(hotel *HotelServer, msg []byte, sig []byte, w http.ResponseWriter
 
 	w.WriteHeader(http.StatusOK)
 	return sendMsg(resp, hotel_comms.MsgType_HOTEL_PING_RESP, w)
+}
+
+func getAction(hotel *HotelServer, msg []byte, sig []byte, w http.ResponseWriter) error {
+	newMsg := &hotel_comms.GetActions{}
+	err := proto.Unmarshal(msg, newMsg)
+	if err != nil {
+		return err
+	}
+
+	actions, err := getActions(hotel.HotelId)
+	if err != nil {
+		return err
+	}
+
+	log.Println(actions)
+
+	resp := &hotel_comms.GetActionsResp{
+		Actions: actions,
+	}
+
+	return sendMsg(resp, hotel_comms.MsgType_GET_ACTIONS_RESP, w)
 }
 
 func main() {
