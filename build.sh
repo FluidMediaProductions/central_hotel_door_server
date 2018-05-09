fd3c4f290e2575a474eae149215c43210d9434c6
Benjamin Misell <benjamin@misell.cymru>
2018-05-09 13:51:56 +0100

    Finish rearanging Dockerfiles

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 HASH=`git rev-parse --verify HEAD`
 
-sudo docker build -t evilben/travelr_auth:$HASH auth/
-sudo docker build -t evilben/travelr_bookings:$HASH bookings/
-sudo docker build -t evilben/travelr_hotels:$HASH hotels/
-sudo docker build -t evilben/travelr_rooms:$HASH rooms/
-sudo docker build -t evilben/travelr_gateway:$HASH gateway/
-sudo docker build -t evilben/travelr_hotel_gateway:$HASH hotel_gateway/
+docker build -t evilben/travelr_auth:$HASH -f Dockerfile.auth ./
+docker build -t evilben/travelr_bookings:$HASH -f Dockerfile.bookings ./
+docker build -t evilben/travelr_hotels:$HASH -f Dockerfile.hotels ./
+docker build -t evilben/travelr_rooms:$HASH -f Dockerfile.rooms ./
+docker build -t evilben/travelr_gateway:$HASH -f Dockerfile.gateway ./
+docker build -t evilben/travelr_hotel_gateway:$HASH -f Dockerfile.hotel_gateway ./
